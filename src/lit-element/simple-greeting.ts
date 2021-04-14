07d8ff7999d9b36bcf9eb85f9a68b8e1ca090b56
Leon Ebbers <lebbers@users.noreply.github.com>
2021-04-14 12:38:26 +0200

    Restructure project

--- src/lit-element/simple-greeting.ts
+++ src/lit-element/simple-greeting.ts
@@ -2,7 +2,7 @@ import { LitElement, html, property, customElement } from "lit-element";
 
 @customElement("simple-greeting")
 export class SimpleGreeting extends LitElement {
-  @property({ attribute: true }) name = "world";
+  @property({ attribute: true }) name = "Person";
 
   render() {
     return html`<p>Hello, ${this.name}!</p>`;
