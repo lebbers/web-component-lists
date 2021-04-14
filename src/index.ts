33ad0a19225ec532429b7af6014af92f4543885a
Leon Ebbers <leonebbers@hotmail.com>
2021-04-14 09:43:03 +0000

    Update files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js";
+import "@webcomponents/webcomponentsjs/webcomponents-bundle.js";
+
 import "./simple-greeting.ts";
 import "./simple-list.ts";
 import "./dynamic-list.ts";
 import "./configurable-list.ts";
 import "./slottable-list.ts";
-import "./lazy-list.ts";
+// import "./lazy-list.ts";
